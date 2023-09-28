c80e5a5a066239ae4cc7e50825cb09ae46ba79ad
Toshi-avicii <tushar.toshi12@gmail.com>
2023-09-28 21:19:22 +0530

    just a trial

--- src/components/menu-tab/TabBar.jsx
+++ src/components/menu-tab/TabBar.jsx
@@ -3,7 +3,7 @@ import Tabs from "./Tabs"
 
 function TabBar() {
   return (
-    <div className="flex items-stretch px-2 md:px-4 sticky top-[84px] md:top-[97px] z-[15] bg-white">
+    <div className="flex items-stretch px-2 md:px-4 sticky top-[84px] md:top-[97px] z-[15] md:z-[5] bg-white">
         <Tabs />
         <Filter />
     </div>
